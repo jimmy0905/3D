38ac3526d00eb970831d4530ba1c3087be150803
jimmy_desktop <fktse10@gmail.com>
2023-09-12 17:30:24 +0800

    9/12

--- src/components/IndoorNavigation.vue
+++ src/components/IndoorNavigation.vue
@@ -20,29 +20,34 @@
             </div>
         </div>
     </div>
-    <!-- <div style="
+    <div style="
       position: absolute;
       z-index: 100;
       background-color: white;
       opacity: 0.5;
       bottom: 0;
     ">
-        <p id="AX" style="color: black; background-color: white">AX:</p>
+        <p id="a">A:</p>
+        <p id="b">B:</p>
+        <!-- <p id="AX" style="color: black; background-color: white">AX:</p>
         <p id="AY" style="color: black; background-color: white">AY:</p>
         <p id="AZ" style="color: black; background-color: white">AZ:</p>
         <p id="camera" style="color: black; background-color: white">X: Y: Z:</p>
         <p id="accDiff" style="color: black; background-color: white">accDiff:</p>
         <p id="Step" style="color: black; background-color: white">Step:</p>
-        <p id="or" style="color: black; background-color: white"></p>
-    </div> -->
+        <p id="or" style="color: black; background-color: white"></p> -->
+    </div>
     <div class="row w-100 fixed-bottom m-0" style="opacity: 0.5; position: absolute;">
-        <button type="button" class="btn btn-primary col" @touchstart="this.movement.rotateLeft = true" @touchend="this.movement.rotateLeft = false"><span class="material-symbols-outlined">
+        <button type="button" class="btn btn-primary col" @touchstart="this.movement.rotateLeft = true"
+            @touchend="this.movement.rotateLeft = false"><span class="material-symbols-outlined">
                 turn_left
             </span></button>
-        <button type="button" class="btn btn-primary col" @touchstart="this.movement.forward = true" @touchend="this.movement.forward = false"><span class="material-symbols-outlined">
+        <button type="button" class="btn btn-primary col" @touchstart="this.movement.forward = true"
+            @touchend="this.movement.forward = false"><span class="material-symbols-outlined">
                 straight
             </span></button>
-        <button type="button" class="btn btn-primary col" @touchstart="this.movement.rotateRight = true" @touchend="this.movement.rotateRight = false"><span class="material-symbols-outlined">
+        <button type="button" class="btn btn-primary col" @touchstart="this.movement.rotateRight = true"
+            @touchend="this.movement.rotateRight = false"><span class="material-symbols-outlined">
                 turn_right
             </span></button>
     </div>
@@ -359,15 +364,17 @@ export default {
                 this.orientation.initialGamma = event.gamma || 0;
             }
             var alpha = (event.alpha || 0) - this.orientation.initialAlpha;
-            var beta = (event.beta || 0) - this.orientation.initialBeta;
-            var gamma = (event.gamma || 0) - this.orientation.initialGamma;
+            // var beta = (event.beta || 0) - this.orientation.initialBeta;
+            // var gamma = (event.gamma || 0) - this.orientation.initialGamma;
+            document.getElementById('a').innerText = "A:" + alpha;
             var tem = alpha;
             alpha = alpha - lastAlpha;
             lastAlpha = tem;
             alpha = THREE.MathUtils.degToRad(alpha);
-            beta = THREE.MathUtils.degToRad(beta);
-            gamma = THREE.MathUtils.degToRad(gamma);
+            //beta = THREE.MathUtils.degToRad(beta);
+            //gamma = THREE.MathUtils.degToRad(gamma);
             //this.camera.rotation.set(beta,alpha,gamma);
+            document.getElementById('b').innerText = "B:" + event.beta;
             this.camera.rotation.y += alpha;
         },
         handleMotion(event) {
